b573cf6ca91fba76878b4a4c8a044929c72d9be1
animesh <animeshrathore255@gmail.com>
2024-08-15 14:31:58 +0530

    updated app

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Weather-App/"
+  build:{
+    target: "esnext" // or "es2019",
+  }
 })
