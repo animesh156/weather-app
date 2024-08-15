e4ab21c63917a944ae4dc60a439597f91b3839c2
animesh <animeshrathore255@gmail.com>
2024-08-15 11:32:58 +0530

    updated app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-/* eslint-disable react/jsx-no-duplicate-props */
-/* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 
 import { WiHumidity } from "react-icons/wi";
 import { FaWind } from "react-icons/fa";
+
 const env = await import.meta.env;
-
-
-import "./App.css";
 
 const api = {
   base: "https://api.openweathermap.org/data/2.5/",
@@ -16,7 +12,8 @@ const api = {
 function App() {
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
-  const [name, setName] = useState("clear");
+  const [name, setName] = useState("");
+
 
   useEffect(() => {
     const city = "Darbhanga";
@@ -28,66 +25,76 @@ function App() {
 
         if (result.weather[0].main === "Clouds") {
           setName("cloudy");
+        } else if (result.weather[0].main === "clear") {
+          setName("clear");
+        } else if (result.weather[0].main === "Rain") {
+          setName("rain");
+        } else if (result.weather[0].main === "Thunderstorm") {
+          setName("thunder");
+        } else if (result.weather[0].main === "Drizzle") {
+          setName("drizzle");
+        } else if (result.weather[0].main === "Snow") {
+          setName("snow");
+        } else if (
+          result.weather[0].main === "Mist" ||
+          result.weather[0].main === "Fog"
+        ) {
+          setName("rain");
+        } else if (result.weather[0].main === "Tornado") {
+          setName("tornado");
+        } else if (
+          result.weather[0].main === "Sand" ||
+          result.weather[0].main === "Dust"
+        ) {
+          setName("sand");
+        } else {
+          setName("clear");
         }
-        else if(result.weather[0].main === "Rain") {
-          setName("rain")
-        }
-        else if(result.weather[0].main === "Thunderstorm") {
-          setName("thunder")
-        }
-        else if(result.weather[0].main === "Drizzle") {
-          setName("drizzle")
-        }
-        else if(result.weather[0].main === "Snow") {
-          setName("snow")
-        }
-        else if(result.weather[0].main === "Mist" || result.weather[0].main === "Fog") {
-          setName("rain")
-        }
-        else if(result.weather[0].main === "Tornado") {
-          setName("tornado")
-        }
-        else if(result.weather[0].main === "Sand"  || result.weather[0].main === "Sand") {
-          setName("sand")
-        }
-
       });
   }, []);
 
   const search = () => {
-    fetch(
-      `${api.base}weather?q=${query}&units=metric&APPID=${env.VITE_API_KEY}`
-    )
-      .then((res) => res.json())
-      .then((result) => {
-        setWeather(result);
-        setQuery("");
-        if (result.weather[0].main === "Clouds") {
-          setName("cloudy");
-        }
-        else if(result.weather[0].main === "Rain") {
-          setName("rain")
-        }
-        else if(result.weather[0].main === "Thunderstorm") {
-          setName("thunder")
-        }
-        else if(result.weather[0].main === "Drizzle") {
-          setName("drizzle")
-        }
-        else if(result.weather[0].main === "Snow") {
-          setName("snow")
-        }
-        else if(result.weather[0].main === "Mist" || result.weather[0].main === "Fog") {
-          setName("rain")
-        }
-        else if(result.weather[0].main === "Tornado") {
-          setName("tornado")
-        }
-        else if(result.weather[0].main === "Sand"  || result.weather[0].main === "Sand") {
-          setName("sand")
-        }
-        
-      });
+    if (!query.trim() || Number.isInteger(parseInt(query))) {
+      alert("Please enter a valid city");
+      setQuery("");
+    } else {
+      fetch(
+        `${api.base}weather?q= ${query}&units=metric&APPID=${env.VITE_API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+
+          if (result.weather[0].main === "Clouds") {
+            setName("cloudy");
+          } else if (result.weather[0].main === "clear") {
+            setName("clear");
+          } else if (result.weather[0].main === "Rain") {
+            setName("rain");
+          } else if (result.weather[0].main === "Thunderstorm") {
+            setName("thunder");
+          } else if (result.weather[0].main === "Drizzle") {
+            setName("drizzle");
+          } else if (result.weather[0].main === "Snow") {
+            setName("snow");
+          } else if (
+            result.weather[0].main === "Mist" ||
+            result.weather[0].main === "Fog"
+          ) {
+            setName("rain");
+          } else if (result.weather[0].main === "Tornado") {
+            setName("tornado");
+          } else if (
+            result.weather[0].main === "Sand" ||
+            result.weather[0].main === "Dust"
+          ) {
+            setName("sand");
+          } else {
+            setName("clear");
+          }
+        });
+    }
   };
 
   const dateBuilder = (d) => {
@@ -123,28 +130,32 @@ function App() {
     return `${day} ${date} ${month} ${year}`;
   };
 
-  
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      search();
+    }
+  };
 
   return (
     <div
-      className="app"
-     style={{backgroundImage: `url("/${name}.gif")` }}
+      className="app h-[540px] w-80  xl:w-96"
+      style={{ backgroundImage: `url("/${name}.gif")` }}
     >
       <main>
         <div className="search-box flex justify-evenly  items-center mt-4">
           <input
             type="text"
-            className="search-bar placeholder-cyan-600"
+            className="search-bar placeholder-cyan-600 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500"
             placeholder="Enter City..."
             onChange={(e) => setQuery(e.target.value)}
-            value={query} 
-        
+            value={query}
+            onKeyDown={handleKeyDown}
+            required
+            name="city"
           />
 
-         
-         
-
           <button
+            id="search-btn"
             type="button"
             className="text-white bg-yellow-400 hover:bg-yellow-500  font-medium rounded-full text-sm px-3 py-2 text-center h-9"
             onClick={search}
@@ -152,27 +163,20 @@ function App() {
             Search
           </button>
         </div>
-        {typeof weather.main != "undefined" ? (
-          <div>
-            <div className="flex justify-center">
-              <img
-                src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`}
-                sizes="50px"
-                alt="wth-img"
-              ></img>
-            </div>
 
+        {typeof weather.main != "undefined" ? (
+          <div className="mt-14">
             <h1 className="temperature text-white">
               {Math.round(weather.main.temp)}°c
             </h1>
 
-            <div className="location-box text-white">
-              <div className="location">
+            <div className="location-box text-white ">
+              <div className="location mt-11">
                 {weather.name}, {weather.sys.country}
               </div>
               <div className="date">{dateBuilder(new Date())}</div>
             </div>
-            <div className="weather-box text-white">
+            <div className="weather-box text-white mt-10">
               <div className="temp">
                 <p className="wind-speed">
                   {Math.round(weather.wind.speed)}km/h{" "}
